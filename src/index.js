41d9b9f3079c0a66ac7738837d818f20f9de2d75
Patriook <94227596+Patriook@users.noreply.github.com>
2022-01-05 10:05:40 +0100

    Szkielet strony
    Stworzenie szkieletu strony

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <div>
+      <h1>Nanogramy</h1>
+      <button id="btnShop">Sklep</button>
+      <div id="mainContener"></div>
+      <footer>
+        <h3>Projekt stworzony przez: Otylie Łagode, Mateusza Majewskiego, Patryka Bojko</h3>
+      </footer>
+    </div>,
+    document.getElementById("root")
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
