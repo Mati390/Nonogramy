0ab0c62981b69f52e9bc239c8c73a8f6a762d1a7
nihilistinen metsuri <73071311+Mati390@users.noreply.github.com>
2022-01-05 10:14:44 +0100

    Add files via upload

--- src/DrawBoard.js
+++ src/DrawBoard.js
@@ -1,31 +1,35 @@
 import React from 'react';
-
-class DrawBoard extends React.Component{
-  constructor(props)
+import './DrawBoard.css';
+function DrawBoard(props) {
+  let Fields = []
+  for (let i = 0; i < props.Fields+2; i++) {
+    Fields[i] = [];
+}
+  for(let i=1; i<props.Fields+1; i++)
   {
-    super(props);
-    this.state ={
-        fields: this.props.fields
-    };
-  }
-  fields(){
+    for(let j=1; j<props.Fields+1; j++)
     {
-      let g = [];
-      for(let a = this.state.fields-1;a<=0;a--  )
-      {
-          g[a] = <div></div>;
-      }
-      return g[1];
+  //numerowanie wierszy(później bedzie wyświetlać ile jest pól do zamalowania)
+  // if(j==1)  Fields[i][j-1] = <span className="right">{i}</span>
+
+    console.log(props.Fields);
+    Fields[i][j] = <div id={i+"x"+j} />
+
+    if(j==props.Fields) Fields[i][j+1] = <br/>
+    
     }
   }
-
-  render(){
-    return(
-      <div>
-     {this.fields()}
-      </div>
-    )
+  //numerowanie kolumn(później bedzie wyświetlać ile jest pól do zamalowania)
+  for(let i=1; i<props.Fields+1; i++)
+  {
+    if(i==props.Fields) Fields[0][i+1] = <br/>
+    Fields[0][i] = <span className="top">{i}</span>
   }
+  return (
+    <div id="DrawBoard">
+        {Fields}
+    </div>
+  );
 }
 
 export default DrawBoard;
