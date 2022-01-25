64b020b8c841aedbfbf6834d8cd6787189764152
Patriook <94227596+Patriook@users.noreply.github.com>
2022-01-25 17:40:44 +0100

    Create Bulb.js

---
+++ src/Bulb.js
@@ -0,0 +1,10 @@
+import './index.css';
+import bulb from './bulb.png';
+function Bulb() {
+    return (
+      <div id="Serce">
+          <img className="bulb" src={bulb}></img>
+      </div>
+    );
+  }
+  export default Bulb;
