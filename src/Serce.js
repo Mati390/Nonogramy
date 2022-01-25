bf0aa28a4189fbf72ea3913f0bbd2abe83c39bd1
Patriook <94227596+Patriook@users.noreply.github.com>
2022-01-25 17:32:29 +0100

    Create Serce.js

---
+++ src/Serce.js
@@ -0,0 +1,10 @@
+import './index.css';
+import heart from './heart.png';
+function Serce() {
+    return (
+      <div id="Serce">
+          <img className="heart" src={heart}></img>
+      </div>
+    );
+  }
+  export default Serce;
