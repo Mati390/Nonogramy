7dad453bdd67ffc7022bc6e2e09451ff3a8fdbc1
Patriook <94227596+Patriook@users.noreply.github.com>
2022-01-25 14:54:15 +0100

    Aktualizacja HTML

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import DrawBoard from './DrawBoard';
+import Draw from './Draw';
+import Count from './Count';
+import Lvl1 from './Lvl1';
 import reportWebVitals from './reportWebVitals';
-ReactDOM.render(
-    <div>
-      <h1>Nanogramy</h1>
-      <button id="btnShop">Sklep</button>
-      <div id="mainContener"></div>
-      <footer>
-        <h3>Projekt stworzony przez: Otylie Łagode, Mateusza Majewskiego, Patryka Bojko</h3>
-      </footer>
-    </div>,
-    document.getElementById("root")
-  );
 
+ReactDOM.render(
+  <React.StrictMode>
+  <div>
+  <h1>Nonogram</h1>
+<button id="btnShop">Shop</button>
+<div id="mainContainer">
+
+  <Lvl1/>
+</div>
+<footer>
+  <h3>Created by: Otylia Łagoda, Mateusz Majewski, Patryk Bojko</h3>
+</footer>
+  </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+Count();
+Draw();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
