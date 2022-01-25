397d5ed7c89d11ed414d778d60e0f2549198616c
Childesimp3000 <82380094+Childesimp3000@users.noreply.github.com>
2022-01-25 15:26:32 +0100

    Update Draw.js

--- src/Draw.js
+++ src/Draw.js
@@ -1,1 +1,17 @@
 
+import './DrawBoard.css';
+function Draw(props) {
+const Fields = [...document.getElementsByClassName("empty")];
+Fields.forEach(field => {
+    field.addEventListener("click",()=>{
+        field.classList.add("answ");
+        if(!field.classList.contains("Correct"))
+        {
+            field.classList.remove("answ");
+            field.classList.add("Wrong");
+        }
+    })
+});
+}
+
+export default Draw;
